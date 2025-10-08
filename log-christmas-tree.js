b1b14283269bd0c491b2fab6665ce4fdbf33296d
marvelken <marvelken2000@gmail.com>
2025-10-08 22:03:34 +0100

    Next Greater Element I

--- log-christmas-tree.js
+++ log-christmas-tree.js
@@ -3,16 +3,11 @@ function christmasTree(width, height) {
   for (i = 0; i < width; i++) {
     console.log(" ".repeat(width - i) + star.repeat(2 * i + 1));
   }
-
   //for the base
-
   for (i = 0; i < 3; i++) {
     console.log(" ".repeat(Number(height))+ star.repeat(height))
   }
   
-
-
 }
 
 christmasTree(11,7);
-
